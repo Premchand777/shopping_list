97bd4b3d9ea3c9322fd18bc0f2bd782ba877de28
Premchand777 <65839523+Premchand777@users.noreply.github.com>
2024-03-25 21:05:52 +0530

    challenge 1 groceries home screen completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/widgets/screens/groceries_home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,22 +11,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Groceries',
-      theme: ThemeData.dark().copyWith(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 147, 229, 250),
-          brightness: Brightness.dark,
-          surface: const Color.fromARGB(255, 42, 51, 59),
+    return SafeArea(
+      child: MaterialApp(
+        title: 'Flutter Groceries',
+        theme: ThemeData.dark().copyWith(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 147, 229, 250),
+            brightness: Brightness.dark,
+            surface: const Color.fromARGB(255, 42, 51, 59),
+          ),
+          scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
         ),
-        scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
+        home: const GroceriesHome(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Flutter Groceries'),
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
